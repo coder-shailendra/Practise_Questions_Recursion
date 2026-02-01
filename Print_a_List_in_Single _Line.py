f49c8a0762c790c135df75694ed33b4ed6cfcdb0
shailendra kumar <shailedrakumar25@gmail.com>
2026-02-01 10:24:47 +0530

    Print_a_List_in_Single _Line.py

---
+++ Print_a_List_in_Single _Line.py
@@ -0,0 +1,11 @@
+cities = ["delhi","gurgaon","noida","pune","mumbai","chennai"]
+heroes = ["salman","shahruk","ahhan","ironman","tiger"]
+
+def print_len(list):
+    print(len(list))
+
+def print_list(list):
+    for items in list:
+        print(items, end = " ")
+print_list(heroes)
+print()
