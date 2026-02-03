774c13ee16408144b3d5f686f6a31b3739c78238
shailendra kumar <shailedrakumar25@gmail.com>
2026-02-04 03:58:09 +0530

    palindrome problem

---
+++ palindrome_by_recursion.py
@@ -0,0 +1,10 @@
+def palindrome_helper(s1,start,end):
+    if (start >= end):
+     return True
+    if (s1[start] != s1[end]):
+       return False
+    return palindrome_helper(s1,start + 1 ,end - 1)
+def palindrome(s1):
+   return palindrome_helper(s1,0,len(s1)-1)
+print(palindrome("nitin"))
+print(palindrome("malayalam"))
